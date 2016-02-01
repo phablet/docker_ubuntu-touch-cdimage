aaef72ef85edef8770cdcc0ac12a6f04bba6566d
You-Sheng Yang <vicamo@gmail.com>
2016-02-01 13:19:46 +0800

    build all at once
    Signed-off-by: You-Sheng Yang <vicamo@gmail.com>

--- update.sh
+++ update.sh
@@ -3,61 +3,53 @@
 cd $(dirname $0)
 set -e
 
-SHA1SUM=${SHA1SUM:-$(which sha1sum)}
-IMAGE_SUITE=$1
-IMAGE_REVISION=$2
-[ -z "${IMAGE_REVISION}" -o -z "${IMAGE_SUITE}" ] && (echo "Missing arguments" >&2; exit 1)
+IMAGE_REPO=phablet/ubuntu-touch-daily-preinstalled
 
-IMAGE_BASE_URL_vivid=http://cdimage.ubuntu.com/ubuntu-touch/vivid/daily-preinstalled/${IMAGE_REVISION}
-IMAGE_BASE_URL_xenial=http://cdimage.ubuntu.com/ubuntu-touch/daily-preinstalled/${IMAGE_REVISION}
-eval "IMAGE_BASE_URL=\${IMAGE_BASE_URL_${IMAGE_SUITE}}"
-[ -z "${IMAGE_BASE_URL}" ] && (echo "Unsupported suite: ${IMAGE_SUITE}" >&2; exit 1)
+IMAGE_BASE_URL_vivid=http://cdimage.ubuntu.com/ubuntu-touch/vivid/daily-preinstalled/current
+IMAGE_BASE_URL_xenial=http://cdimage.ubuntu.com/ubuntu-touch/daily-preinstalled/current
 
-CUT_DIRS=$(echo ${IMAGE_BASE_URL} | tr / ' ' | wc -w)
-CUT_DIRS=$((${CUT_DIRS} - 2))
+QEMU_ARCH_armhf=arm
+QEMU_ARCH_i386=i386
 
-function do_build() {
-  local arch=$1
-  local qemu_arch=$2
-  qemu_arch=${qemu_arch:-${arch}}
+do_update() {
+  local suite=$1
+  local arch=$2
 
-  local rootfs_dir=${IMAGE_SUITE}/rootfs
-  local custom_dir=${IMAGE_SUITE}/custom
-  local rootfs_tar_name=${IMAGE_SUITE}-preinstalled-touch-${arch}
-  local custom_tar_name=${IMAGE_SUITE}-preinstalled-touch-${arch}.custom
-  mkdir -p ${rootfs_dir} ${custom_dir}
-  (cd ${rootfs_dir}; wget --timestamping --continue ${IMAGE_BASE_URL}/${rootfs_tar_name}.tar.gz)
-  (cd ${custom_dir}; wget --timestamping --continue ${IMAGE_BASE_URL}/${custom_tar_name}.tar.gz)
+  local rootfs_dir=${suite}/${arch}
+  local custom_dir=${rootfs_dir}/custom
+  local rootfs_tar_name=${suite}-preinstalled-touch-${arch}
+  local custom_tar_name=${suite}-preinstalled-touch-${arch}.custom
+  local image_id_rootfs=${IMAGE_REPO}:${rootfs_tar_name}
+  local image_id_custom=${IMAGE_REPO}:${custom_tar_name}
 
-  local image_repo=phablet/ubuntu-touch-cdimage
-  local image_id_rootfs=${image_repo}:${rootfs_tar_name}
-  local image_id_custom=${image_repo}:${custom_tar_name}
+  eval "local qemu_arch=\${QEMU_ARCH_${arch}}"
+  [ -z "${qemu_arch}" ] && (echo "Unsupported arch: ${arch}" >&2; exit 1)
 
-  local IMAGE_SHA1SUM=$(${SHA1SUM} ${rootfs_dir}/${rootfs_tar_name}.tar.gz | awk '{print $1}')
+  eval "local base_url=\${IMAGE_BASE_URL_${suite}}"
+  [ -z "${base_url}" ] && (echo "Unsupported suite: ${suite}" >&2; exit 1)
+
+  (cd ${rootfs_dir}; wget --timestamping --continue ${base_url}/${rootfs_tar_name}.tar.gz)
+  (cd ${custom_dir}; wget --timestamping --continue ${base_url}/${custom_tar_name}.tar.gz)
+
   cat Dockerfile.template | \
-      sed -e "s,@IMAGE_REVISION@,${IMAGE_REVISION},g" \
-        -e "s,@IMAGE_SHA1SUM@,${IMAGE_SHA1SUM},g" \
-        -e "s,@IMAGE_BASE_URL@,${IMAGE_BASE_URL},g" \
-        -e "s,@IMAGE_SUITE@,${IMAGE_SUITE},g" \
+      sed -e "s,@IMAGE_BASE_URL@,${base_url},g" \
+        -e "s,@IMAGE_SUITE@,${suite},g" \
         -e "s,@IMAGE_ARCH@,${arch},g" \
         -e "s,@IMAGE_QEMU_ARCH@,${qemu_arch},g" \
       > ${rootfs_dir}/Dockerfile \
-    && docker build -t ${image_id_rootfs} ${rootfs_dir} \
-    && docker tag -f ${image_id_rootfs} ${image_id_rootfs}.${IMAGE_REVISION} \
-    && docker tag -f ${image_id_rootfs} ${image_id_rootfs}.${IMAGE_SHA1SUM}
+    && docker build -t ${image_id_rootfs} ${rootfs_dir}
 
-  local IMAGE_SHA1SUM=$(${SHA1SUM} ${custom_dir}/${custom_tar_name}.tar.gz | awk '{print $1}')
   cat Dockerfile.custom.template | \
-      sed -e "s,@IMAGE_REVISION@,${IMAGE_REVISION},g" \
-        -e "s,@IMAGE_SHA1SUM@,${IMAGE_SHA1SUM},g" \
-        -e "s,@IMAGE_BASE_URL@,${IMAGE_BASE_URL},g" \
-        -e "s,@IMAGE_SUITE@,${IMAGE_SUITE},g" \
+      sed -e "s,@IMAGE_BASE_URL@,${base_url},g" \
+        -e "s,@IMAGE_SUITE@,${suite},g" \
         -e "s,@IMAGE_ARCH@,${arch},g" \
       > ${custom_dir}/Dockerfile \
-    && docker build -t ${image_id_custom} ${custom_dir} \
-    && docker tag -f ${image_id_custom} ${image_id_custom}.${IMAGE_REVISION} \
-    && docker tag -f ${image_id_custom} ${image_id_custom}.${IMAGE_SHA1SUM}
+    && docker build -t ${image_id_custom} ${custom_dir}
 }
 
-do_build i386
-do_build armhf arm
+for target in $(find . -maxdepth 2 -mindepth 2 -type d | grep -v git);
+do
+  suite=$(echo $target | cut -d/ -f2)
+  arch=$(echo $target | cut -d/ -f3)
+  do_update $suite $arch
+done
